c5e172708b23023318f78c9bcafbff20ec3a9b56
jlarsen <jlarsen@usgs.gov>
2024-06-01 06:24:25 -0700

    Continue packaging D-Any

--- dany/__init__.py
+++ dany/__init__.py
@@ -1,3 +1,4 @@
+from .version import __version__
 from .dem_conditioning import fill_sinks, fill_nan_values
 from .flow_directions import FlowDirections
 from .stream_util import Sfr6, Sfr2005, PrmsStreams
