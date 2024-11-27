783faacbd48b8a35d5a91daedb671bcc17ceda93
jlarsen <jlarsen@usgs.gov>
2024-11-27 13:30:20 -0800

    Added data and additional tests for FlowDirections() class routines

--- autotest/test_flow_directions.py
+++ autotest/test_flow_directions.py
@@ -49,7 +49,6 @@ def test_flow_directions_unfilled():
 
     fdobj = dany.FlowDirections(grid, dem)
     fdir = fdobj.flow_directions()
-    np.savetxt(fdir_file, fdir, fmt="%d")
 
     if not np.allclose(fdir, valid_fdirs):
         raise AssertionError(
@@ -58,22 +57,101 @@ def test_flow_directions_unfilled():
 
 
 def test_get_watershed():
-    pass
+    watershed_file = data_ws / "watershed_boundary.txt"
+    valid_wshed = np.genfromtxt(watershed_file, dtype=int)
+
+    fdobj = dany.FlowDirections(grid, dem)
+    fdir = fdobj.flow_directions()
+    point = (grid.xcellcenters[39, 15], grid.ycellcenters[39, 15])
+
+    watershed = fdobj.get_watershed_boundary(point)
+
+    if not np.allclose(watershed, valid_wshed):
+        raise AssertionError(
+            "get_watershed not calculating the correct divide locations"
+        )
 
 
 def test_get_subbassins():
-    pass
+    subbasin_file = data_ws / "subbasins.txt"
+    valid_subs = np.genfromtxt(subbasin_file, dtype=int)
+
+    locs = [[39, 15], [30, 12], [29, 13]]
+    points = [[grid.xcellcenters[r, c], grid.ycellcenters[r, c]] for r, c in locs]
+
+    fdobj = dany.FlowDirections(grid, dem)
+    fdir = fdobj.flow_directions()
+    subbasins = fdobj.get_subbasins(points)
+
+    if not np.allclose(subbasins, valid_subs):
+        raise AssertionError(
+            "get_subbasins not calculating the correct contrib. areas"
+        )
 
 
-def test_flow_accumulation():
-    pass
+def test_flow_accumulation_as_area():
+    fa_file = data_ws / "flow_accumulation_area.txt"
+    valid_fa = np.genfromtxt(fa_file, dtype=int)
+
+    fdobj = dany.FlowDirections(grid, dem)
+    fdir = fdobj.flow_directions()
+    facc = fdobj.flow_accumulation()
+
+
+    if not np.allclose(facc, valid_fa):
+        raise AssertionError(
+            "flow accumulation as area not returning correct values"
+        )
+
+
+def test_flow_accumulation_as_cells():
+    fa_file = data_ws / "flow_accumulation_as_cells.txt"
+    valid_fa = np.genfromtxt(fa_file, dtype=int)
+
+    fdobj = dany.FlowDirections(grid, dem)
+    fdir = fdobj.flow_directions()
+    facc = fdobj.flow_accumulation(as_cells=True)
+
+    if not np.allclose(facc, valid_fa):
+        raise AssertionError(
+            "flow accumulation as cells not returning correct values"
+        )
 
 
 def test_flow_direction_attributes():
-    pass
+    slopes_file = data_ws / "slope.txt"
+    aspect_file = data_ws / "aspect.txt"
+    hru_len_file = data_ws / "hru_len.txt"
+    vu_file = data_ws / "vector_u_file.txt"
+    vv_file = data_ws / "vector_v_file.txt"
 
+    valid_slope = np.genfromtxt(slopes_file)
+    valid_aspect = np.genfromtxt(aspect_file)
+    valid_hru_len = np.genfromtxt(hru_len_file)
+    valid_vu = np.genfromtxt(vu_file)
+    valid_vv = np.genfromtxt(vv_file)
 
-if __name__ == "__main__":
-    test_flow_directions_filled()
-    test_flow_directions_unfilled()
+    fdobj = dany.FlowDirections(grid, dem)
+    fdir = fdobj.flow_directions()
 
+    if not np.allclose(fdobj.slope, valid_slope):
+        raise AssertionError(
+            "Slope calculation is not correct"
+        )
+
+    if not np.allclose(fdobj.aspect, valid_aspect):
+        raise AssertionError(
+            "Aspect calculation is not correct"
+        )
+
+    if not np.allclose(fdobj.hru_len, valid_hru_len):
+        raise AssertionError(
+            "hru_len calculation is not correct"
+        )
+
+    u, v = fdobj.vectors
+
+    if not np.allclose(u, valid_vu) or not np.allclose(v, valid_vv):
+        raise AssertionError(
+            "unit circle vector calculation is not correct"
+        )
